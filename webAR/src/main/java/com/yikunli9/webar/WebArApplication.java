cca1e076b9ed035fadd491ae9ac04bfc74d73b81
YikunLi <yikun.li19@gmail.com>
2023-10-30 00:27:28 +0000

    WebAR implementation(First commit)

---
+++ webAR/src/main/java/com/yikunli9/webar/WebArApplication.java
@@ -0,0 +1,13 @@
+package com.yikunli9.webar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebArApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebArApplication.class, args);
+    }
+
+}
---
+++ webAR/src/test/java/com/yikunli9/webar/WebArApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yikunli9.webar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebArApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
